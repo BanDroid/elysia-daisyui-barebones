927b1e7e2235164fdf51ef527137ea4417c91db0
dbbhuh5 <dbbhuh5@gmail.com>
2025-10-18 09:34:25 +0000

    feat: firebase studio support

---
+++ .idx/dev.nix
@@ -0,0 +1,20 @@
+{ pkgs, ... }: {
+  # Which nixpkgs channel to use.
+  channel = "stable-24.11"; # or "unstable"
+
+  # Use https://search.nixos.org/packages to find packages
+  packages = [
+    pkgs.nodejs_20
+  ];
+  idx.workspace.onCreate = {
+    bun-setup = "curl -fsSL https://bun.sh/install | bash && bun install";
+    # files to open when the workspace is first opened.
+    default.openFiles = [ "src/index.ts" ];
+  };
+  idx.extensions = [
+    "esbenp.prettier-vscode"
+    "dbcode.dbcode"
+    "pkief.material-icon-theme"
+    "prisma.prisma"
+  ];
+}
